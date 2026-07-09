e0e606bb09549b46789b48f85eeda4a227600ca6
jjvazquezsmateo-web <jjvazquezsmateo@gmail.com>
2026-07-09 12:24:41 -0400

    Update bot activity message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Helping with the Military, Good luck Soilders",
         // Activity type number (0 = Playing).
         type: 0,
       },
