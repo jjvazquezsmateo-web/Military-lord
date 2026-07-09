a1305bd6a4319c345c709c1ad44f2ede9af669ad
jjvazquezsmateo-web <jjvazquezsmateo@gmail.com>
2026-07-09 12:27:57 -0400

    Add emoji to activity name in bot configuration

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Helping with the Military, Good luck Soilders",
+        name: "Helping with the Military, Good luck Soilders⚔️",
         // Activity type number (0 = Playing).
         type: 0,
       },
